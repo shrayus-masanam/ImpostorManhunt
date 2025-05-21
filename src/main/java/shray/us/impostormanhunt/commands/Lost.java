600d3bc62fc8663fe99d1991db03110897002042
shrayus-masanam <45981228+shrayus-masanam@users.noreply.github.com>
2025-05-21 16:07:42 -0400

    create lost cmd

--- src/main/java/shray/us/impostormanhunt/commands/Lost.java
+++ src/main/java/shray/us/impostormanhunt/commands/Lost.java
@@ -1,4 +1,30 @@
 package shray.us.impostormanhunt.commands;
 
-public class Lost {
+import org.bukkit.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class Lost implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender,
+                             @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
+
+        if (!(commandSender instanceof Player player)) {
+            commandSender.sendMessage(ChatColor.RED + "You must be a player to use this command.");
+            return true;
+        }
+        if (player.getGameMode() == GameMode.SPECTATOR) {
+            player.sendMessage(ChatColor.RED + "You can't use this command as a spectator.");
+            return true;
+        }
+
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            p.sendMessage(p.getName() + " is lost and needs help!");
+        }
+
+        return true;
+    }
 }
