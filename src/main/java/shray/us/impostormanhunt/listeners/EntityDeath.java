27cea7f4ea53e5b5070568b0945d0cb6ebaf0b00
shrayus-masanam <45981228+shrayus-masanam@users.noreply.github.com>
2025-05-22 00:05:29 -0400

    add listener for game stop

--- src/main/java/shray/us/impostormanhunt/listeners/EntityDeath.java
+++ src/main/java/shray/us/impostormanhunt/listeners/EntityDeath.java
@@ -1,4 +1,37 @@
 package shray.us.impostormanhunt.listeners;
 
-public class EntityDeath {
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.entity.EnderDragon;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import shray.us.impostormanhunt.structures.Competitor;
+import shray.us.impostormanhunt.structures.Game;
+
+public class EntityDeath implements Listener {
+
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent event) {
+        if (event.getEntity() instanceof EnderDragon) {
+            Game.stop(true);
+        } else if (event.getEntity() instanceof Player) {
+            Competitor competitor = Game.getCompetitor((Player) event.getEntity());
+            if (competitor == null) return;
+            if (!competitor.isImpostor())
+                competitor.eliminate();
+            boolean allEliminated = true;
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                competitor = Game.getCompetitor((Player) event.getEntity());
+                if (competitor == null || competitor.isImpostor()) continue;
+                if (!competitor.isEliminated()) {
+                    allEliminated = false;
+                    break;
+                }
+            }
+            if (allEliminated)
+                Game.stop(false);
+        }
+    }
 }
