5bad15f8106821de85a36aedf4f5a95249521dc7
shrayus-masanam <45981228+shrayus-masanam@users.noreply.github.com>
2025-05-22 11:06:19 -0400

    spectator-only chat

--- src/main/java/shray/us/impostormanhunt/listeners/ChatEvent.java
+++ src/main/java/shray/us/impostormanhunt/listeners/ChatEvent.java
@@ -1,4 +1,27 @@
 package shray.us.impostormanhunt.listeners;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import shray.us.impostormanhunt.structures.Competitor;
+import shray.us.impostormanhunt.structures.Game;
+
 public class ChatEvent {
+
+    @EventHandler
+    public void onMessage(AsyncPlayerChatEvent event) {
+        Competitor c = Game.getCompetitor(event.getPlayer());
+        if (c == null) return;
+        if (!c.isEliminated()) {
+            event.getRecipients().clear();
+            event.setCancelled(true);
+        } else {
+            for (Player p : event.getRecipients()) {
+                Competitor c2 = Game.getCompetitor(p);
+                if (c2 == null) continue;
+                if (!c2.isEliminated())
+                    event.getRecipients().remove(p);
+            }
+        }
+    }
 }
