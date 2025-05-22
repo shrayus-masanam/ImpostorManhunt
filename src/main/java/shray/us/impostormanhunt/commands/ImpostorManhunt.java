7df5a720ef2bf8420856684f1cb1c757ac3db72f
shrayus-masanam <45981228+shrayus-masanam@users.noreply.github.com>
2025-05-22 11:05:59 -0400

    set gamerules for all worlds on start

--- src/main/java/shray/us/impostormanhunt/commands/ImpostorManhunt.java
+++ src/main/java/shray/us/impostormanhunt/commands/ImpostorManhunt.java
@@ -1,15 +1,21 @@
 package shray.us.impostormanhunt.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import org.bukkit.command.ConsoleCommandSender;
 import org.jetbrains.annotations.NotNull;
 import shray.us.impostormanhunt.structures.Game;
+import shray.us.impostormanhunt.utils.WorldName;
 
 public class ImpostorManhunt implements CommandExecutor {
+
+    private final ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+
+
     public boolean onCommand(@NotNull CommandSender commandSender,
                              @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] args) {
         if (!commandSender.isOp()) {
@@ -19,6 +25,13 @@ public class ImpostorManhunt implements CommandExecutor {
 
         if (args[0].equalsIgnoreCase("start")) {
             Game.start();
+            for (World w : Bukkit.getWorlds()) {
+                Bukkit.dispatchCommand(console, "execute in "
+                        + WorldName.toDimension(w) + " run gamerule announceAdvancements false");
+                Bukkit.dispatchCommand(console, "execute in "
+                        + WorldName.toDimension(w) + " run gamerule showDeathMessages false");
+            }
+            commandSender.sendMessage(ChatColor.GREEN + "Game started.");
         }
 
         return true;
