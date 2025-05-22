3e00e9f71005230ae88f78f114761f0329a38b14
shrayus-masanam <45981228+shrayus-masanam@users.noreply.github.com>
2025-05-22 00:06:03 -0400

    handle player respawn by role

--- src/main/java/shray/us/impostormanhunt/listeners/PlayerRespawn.java
+++ src/main/java/shray/us/impostormanhunt/listeners/PlayerRespawn.java
@@ -1,16 +1,16 @@
 package shray.us.impostormanhunt.listeners;
 
 import com.destroystokyo.paper.event.player.PlayerPostRespawnEvent;
-import org.bukkit.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.Listener;
 import shray.us.impostormanhunt.Main;
 import shray.us.impostormanhunt.structures.Competitor;
 import shray.us.impostormanhunt.structures.Game;
 
-public class PlayerDeath {
+public class PlayerRespawn implements Listener {
 
     @EventHandler
     public void onPlayerDeath(PlayerPostRespawnEvent event) {
@@ -22,6 +22,12 @@ public class PlayerDeath {
 
             if (competitor.isImpostor()) {
                 player.sendMessage(Main.prefix + "You will respawn in 30 seconds.");
+                Bukkit.getScheduler().runTaskLater(Main.getInstance(), new Runnable() {
+                    @Override
+                    public void run() {
+                        player.setGameMode(GameMode.SURVIVAL);
+                    }
+                }, 20L * 2);
             } else {
                 player.sendMessage(Main.prefix + "You have been eliminated. You can now speak with other spectators "
                         + "in text or voice chat.");
