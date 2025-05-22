3c33f4cea17fc6d27dcf356713dfb7898840d370
shrayus-masanam <45981228+shrayus-masanam@users.noreply.github.com>
2025-05-22 00:05:01 -0400

    add game stop method

--- src/main/java/shray/us/impostormanhunt/structures/Game.java
+++ src/main/java/shray/us/impostormanhunt/structures/Game.java
@@ -2,6 +2,8 @@ package shray.us.impostormanhunt.structures;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 import org.bukkit.entity.Player;
 import shray.us.impostormanhunt.Main;
 
@@ -28,7 +30,18 @@ public class Game {
         }
         Game.ongoing = true;
     }
-    public static void stop() {
+    public static void stop(boolean runnersWon) {
+        String title = "";
+        if (runnersWon) {
+            title = ChatColor.GREEN + "Runners Win!";
+        } else {
+            title = ChatColor.RED + "Impostors Win!";
+        }
+        for (Competitor competitor : competitors) {
+            competitor.getPlayer().sendMessage(title);
+            if (!runnersWon)
+                competitor.getPlayer().playSound(competitor.getPlayer(), Sound.ENTITY_WITHER_SPAWN, 1.0F, 1.0F);
+        }
         Game.ongoing = false;
         competitors = null;
     }
