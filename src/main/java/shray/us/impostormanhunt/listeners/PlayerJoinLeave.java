9eb845f60f310d7a71a3e1412a9b822f8544b5f7
shrayus-masanam <45981228+shrayus-masanam@users.noreply.github.com>
2025-05-30 15:36:34 -0400

    hide join/leave messages for competitors

--- src/main/java/shray/us/impostormanhunt/listeners/PlayerJoinLeave.java
+++ src/main/java/shray/us/impostormanhunt/listeners/PlayerJoinLeave.java
@@ -6,17 +6,17 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import shray.us.impostormanhunt.structures.Game;
 
-public class PlayerLeave implements Listener {
-
-    @EventHandler
-    public void onPlayerLeave(PlayerQuitEvent event) {
-        if (Game.isOngoing() && Game.getCompetitor(event.getPlayer()) != null) {}
-            event.setQuitMessage("");
-    }
+public class PlayerJoinLeave implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         if (Game.isOngoing() && Game.getCompetitor(event.getPlayer()) != null)
             event.setJoinMessage("");
+    }
+
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent event) {
+        if (Game.isOngoing() && Game.getCompetitor(event.getPlayer()) != null) {}
+            event.setQuitMessage("");
     }
 }
